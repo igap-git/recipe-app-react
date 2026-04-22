24f11aef1eef690cb952098a1f2d544583be84d9
igap-git <igapermail@gmail.com>
2026-04-22 11:44:17 +0200

    feat: update app structure and entry point

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,12 @@
 import Button from "../components/Button/Button"
+import Card from "../components/Card/Card"
 import React from "react";
 
 export default function App(){
     return(
+        <>
         <Button variant="primary">Save</Button>
+        <Card SourceImage={'https://img.freepik.com/darmowe-zdjecie/pizza-pizza-wypelniona-pomidorami-salami-i-oliwkami_140725-1200.jpg?semt=ais_hybrid&w=740&q=80'} title="Pizza a'la petrola"></Card>
+        </>
     );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./app/App";
+import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 
